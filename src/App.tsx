567a8a111ac19367f59c125b79bf0d38eb732493
aaceneelyan <arcrxx@gmail.com>
2025-07-19 22:40:01 +0530

    Fix build error: Remove Received component import and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
 import ThankYou from "./pages/ThankYou";
 import NotFound from "./pages/NotFound";
-import Received from "./pages/Received";
 import { AuthProvider } from "./contexts/AuthContext";
 
 const queryClient = new QueryClient();
@@ -24,7 +22,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/thankyou" element={<ThankYou />} />
-            <Route path="/received" element={<Received />} />
+            <Route path="/received" element={<Navigate to="/received.html" replace />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
